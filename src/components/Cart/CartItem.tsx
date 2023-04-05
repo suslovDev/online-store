e450c67cfcf4a97695ce3e8c42f8ac62e410c575
SuslovDev <Suslova-valerija@rambler.ru>
2023-04-05 22:31:00 +0300

    put in order CartItem.tsx

--- src/components/Cart/CartItem.tsx
+++ src/components/Cart/CartItem.tsx
@@ -1,25 +1,25 @@
 import { FC } from "react";
+import classes from "./CartItem.module.css";
 import AmountControl from "../UI/AmountControl";
 import Button from "../UI/Button";
-import classes from "./CartItem.module.css";
-import { IProductItem } from "../Product/IProductItem";
 import Price from "../UI/Price";
 import ProductSize from "../Product/ProductSize";
-
+import { IProductItem } from "../Product/IProductItem";
 import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
-import { plusToCart, minusFromCart, removeFromCart } from "../../store/cart-slice";
+import {
+  plusToCart,
+  minusFromCart,
+  removeFromCart,
+} from "../../store/cart-slice";
 
 const CartItem: FC<{ item: IProductItem }> = ({ item }) => {
   const { cart } = useAppSelector((state) => state.cart);
+
   const dispatch = useAppDispatch();
-  
-   let el = cart.find((i) => i.barcode == item.barcode);
-   let initialAmount;
-   if (el) {
-    initialAmount = el.quantity;
-   } else {
-    initialAmount = 1;
-   }
+
+  let currentCartItem = cart.find((i) => i.barcode == item.barcode);
+
+  let initialAmount = currentCartItem ? currentCartItem.quantity : 1;
 
   return (
     <div className={classes.item}>
