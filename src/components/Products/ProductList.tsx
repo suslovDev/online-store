2f56724aca04585d5d325cee5938b9a73e0ba6de
SuslovDev <Suslova-valerija@rambler.ru>
2023-04-07 16:54:14 +0300

    cansel: now work just 3 tests

--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -1,22 +1,13 @@
 import { FC } from "react";
 import classes from "./ProductList.module.css";
-import { useAppDispatch } from "../../hooks/hooks";
 import { IProductList } from "./IProductList";
 import ProductItem from "../Product/product-item/ProductItem";
-import { plusToCart } from "../../store/cart-slice";
 
 const ProductList: FC<IProductList> = ({ products }) => {
-  //const dispatch = useAppDispatch();
   return (
-    <div data-testid='list' className={classes.list}>
+    <div className={classes.list}>
       {products.map((product) => {
-        return (
-          <ProductItem
-            item={product}
-           // onBtnClick={dispatch(plusToCart({ product: product, quantity: 1 }))}
-            key={product.barcode}
-          />
-        );
+        return <ProductItem item={product} key={product.barcode} />;
       })}
     </div>
   );
